fbf609caa4f677dfb0b202abc1d8f90229360eab
Tobi <47112773+Tobylarry@users.noreply.github.com>
2023-10-01 23:24:44 +0000

    up

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json())
+app.set('view engine', 'ejs')
 app.use(express.static("public"))
 const stripe = require('stripe')(process.env.ddjjjf)
 
