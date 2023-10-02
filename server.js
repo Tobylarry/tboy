78ed8c3ad0a14d8e794b4372d5f3739561ae7bc4
Tobi <47112773+Tobylarry@users.noreply.github.com>
2023-10-02 00:02:11 +0000

    u

--- server.js
+++ server.js
@@ -9,9 +9,21 @@ app.set('view engine', 'ejs')
 app.use(express.static("public"))
 const stripe = require('stripe')(process.env.ddjjjf)
 
-const items = new Map([]);
+const items = JSON.parse(localStorage.getItem('cart'));
 
-app.post('/create-checkout-session', (req, res) => {
+app.post('/create-checkout-session',async  (req, res) => {
+    try{
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        mode: 'payment',
+        line_items: req.body.items,
+        success_url: `${process.env.SERVER_URL}/success.html`,
+        cancel_url: `${process.env.SERVER_URL}/cancel.html`
+      })
+    }
+    catch(e){
+        res.status(500).json({ error: e.message })
+    }
     res.json( { url: 'HI' })
 })
 app.listen(4000)
